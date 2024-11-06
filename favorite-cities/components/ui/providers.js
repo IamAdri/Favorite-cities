7768ae9e2cc18f905203c2dc76ac7a50d836a209
IamAdri <atoma304@gmail.com>
2024-11-06 12:19:06 +0200

    Setup finished

--- favorite-cities/components/ui/providers.js
+++
@@ -1,7 +0,0 @@
-"use client"
-import { ThemeProvider } from "next-themes";
-import { useEffect, useState } from "react";
-
-const Providers = ({children}) => {
-    const [mounted, setMounted] = 
-}
--- favorite-cities/pages/index.js
+++ favorite-cities/pages/index.js
@@ -1,19 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
-
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
@@ -24,10 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-   
       <div>Hello</div>
-    
-      
     </>
   );
 }
